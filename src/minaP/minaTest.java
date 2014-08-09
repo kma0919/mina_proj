88a7f1d78dd6015073a6ef6c6929ab4975a8482f
kma0919 <kma0919@gmail.com>
2014-08-09 17:40:59 +0900

    Update minaTest.java

--- src/minaP/minaTest.java
+++ src/minaP/minaTest.java
@@ -4,13 +4,13 @@ public class minaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("spring을 사랑하는 fall_옹옹냐abnnnnc");
+		System.out.println("spring1 server1");
 		
-		System.out.println("spring을 사랑하는 fall_옹옹냐abnnnnc1212");
+		System.out.println("spring1 server2");
 		
-		System.out.println("spring을 사랑하는 ggg fall_옹옹냐a 추가추가ncaa111");
+		System.out.println("spring1 server3");
 		
-		System.out.println("spring을 사랑하는 ggg fall_옹옹냐a 추가추가ncaa111111111");
+		
 	}
 
 
