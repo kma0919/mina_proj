065ea66596e333a85adbe88be4536267dd9905b3
sangnam18 <sangnam18@nate.com>
2014-08-09 16:20:05 +0900

    Update minaTest.java

--- src/minaP/minaTest.java
+++ src/minaP/minaTest.java
@@ -4,7 +4,7 @@ public class minaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("spring을 사랑하는 fall");
+		System.out.println("spring을 사랑하는 fallsss");
 	}
 
 
